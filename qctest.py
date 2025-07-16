622a5118eb56bc12f6bb7176187fc36f9aee1cf3
Smeeton <smeeton2@gmail.com>
2025-07-16 13:48:01 -0400

    added tests for gates

--- qctest.py
+++ qctest.py
@@ -1,6 +1,7 @@
 #unit test for qcempy
 
 import qcempy as qc
+import qgempy as qg
 import numpy as np
 import pytest
 
@@ -19,3 +20,22 @@ def test_MPS():
     assert (MPStest.order[1] == [1]).all()
     assert (MPStest.order[2] == [2]).all()
     assert MPStest.error == 0.01
+
+def test_single_gate_get():
+    assert (qg.get_single_gate('X') == np.array([[0, 1], [1, 0]])).all()
+    assert (qg.get_single_gate('Y') == np.array([[0, -1j], [1j, 0]])).all()
+    assert (qg.get_single_gate('Z') == np.array([[1, 0], [0, -1]])).all()
+    assert (qg.get_single_gate('H') == np.array([[1, 1], [1, -1]])/np.sqrt(2)).all()
+    assert (qg.get_single_gate('S') == np.array([[1, 0], [0, 1j]])).all()
+    assert (qg.get_single_gate('T') == np.array([[1, 0], [0, np.exp(1j*np.pi/4)]])).all()
+    assert (qg.get_single_gate('I') == np.array([[1, 0], [0, 1]])).all()
+
+def test_double_gate_get():
+    assert (qg.get_double_gate('CX') == np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 1], [0, 0, 1, 0]])).all()
+    assert (qg.get_double_gate('CY') == np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, -1j], [0, 0, 1j, 0]])).all()
+    assert (qg.get_double_gate('CZ') == np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, -1]])).all()
+    assert (qg.get_double_gate('CH') == np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 1], [0, 0, 1, -1]])/np.sqrt(2)).all()
+    assert (qg.get_double_gate('CS') == np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1j]])).all()
+    assert (qg.get_double_gate('CT') == np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, np.exp(1j*np.pi/4)]])).all()
+    assert (qg.get_double_gate('SWAP') == np.array([[1, 0, 0, 0], [0, 0, 1, 0], [0, 1, 0, 0], [0, 0, 0, 1]])).all()
+    assert (qg.get_double_gate('CI') == np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])).all()
