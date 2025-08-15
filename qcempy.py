6cf7190379abbf481110f4edf43399cfe0a6de58
Smeeton <smeeton2@gmail.com>
2025-08-15 13:16:22 -0400

    did debugging

--- qcempy.py
+++ qcempy.py
@@ -20,17 +20,18 @@ class MPS:
 
     def add_single_gate(self, qbit, gate):
       # function to add a gate to one qubit.
-      indices = np.where(self.order == qbit)
-      if gate.isstring:
+      indices = np.where(self.order == qbit)[0][0]
+      if isinstance(gate, str):
             gate = qg.get_single_gate(gate)
-      sedlf.qbit[indices]= np.dot(gate, self.qbit[indices])
+      self.qbit[indices]= np.dot(gate, self.qbit[indices])
 
     def svd_2qubit(self,indices1, indices2, gb2):
         #function to perform svd on a 2 qbit tensor
         U,S,V =np.linalg.svd(gb2 , full_matrices=False)
         self.qbit[indices1] = U[:,0] 
-        self.Lambda[indices1] = S[0] 
+        self.Lambda[indices1] = np.array([S[0]]) 
         self.qbit[indices2] = V[0,:]
+       
 
     def combine_two_qubits(self, indices1, indices2):
         #function to combine two qubits
@@ -39,22 +40,24 @@ class MPS:
         else:
             return np.tensordot(np.tensordot(self.qbit[indices1],self.Lambda[indices1],1),self.qbit[indices2],1)
         
-       
 
     def swap_qubits(self, indices1, indices2):
         #function to swap two qubits
         gate = qg.get_double_gate("SWAP")
-        gb2 = self.combine_two_qubits(indices1, indices2)
+        qb2 = self.combine_two_qubits(indices1, indices2)
+        qb2 = np.concatenate((qb2[0], qb2[1]))
+        qb2 = np.concatenate((qb2[0], qb2[1]))
         qb2 = np.dot(gate, qb2)
-        self.svd_2qubit(indices1, indices2, gb2)
+        qb2 = np.array(np.split(qb2, 2))
+        self.svd_2qubit(indices1, indices2, qb2)
         hold = self.order[indices2]
         self.order[indices2] = self.order[indices1]
         self.order[indices1] = hold
 
     def add_double_gate(self, qbit1, qbit2, gate):
         #function to add a gate that effects two qubits
-        indices1 = np.where(self.order == qbit1)
-        indices2 = np.where(self.order == qbit2)
+        indices1 = np.where(self.order == qbit1)[0][0]
+        indices2 = np.where(self.order == qbit2)[0][0]
         if abs(indices1 - indices2) > 1:
             swapindices = min(indices1, indices2)
             finalindices = max(indices1, indices2)
@@ -71,32 +74,34 @@ class MPS:
         if indices1 > indices2:
             self.swap_qubits(indices2, indices1)
 
-        if gate.isstring:
+        if isinstance(gate, str):
             gate = qg.get_double_gate(gate)
-
+        
         qb2 = self.combine_two_qubits(indices1, indices2)
+        qb2 = np.concatenate((qb2[0], qb2[1]))
+        qb2 = np.concatenate((qb2[0], qb2[1]))
         qb2 = np.dot(gate, qb2)
-        self.svd_2qubit(indices1, indices2, gb2)
+        qb2 = np.array(np.split(qb2, 2))
+        self.svd_2qubit(indices1, indices2, qb2)
 
     def reorder(self):
         #reoder the qubits to orginal order
         while np.all(np.diff(self.order) <= 0):
-            for i in range(len(self.N)):
-                place = np.where(self.order == i)
+            for i in range(self.N):
+                place = np.where(self.order == i)[0][0]
                 if place != i:
                     if place > i:
-                        start = i
-                        end   = place
+                        for j in range(place,i+1, -1):
+                            self.swap_qubits(j-1, j)
                     else:
-                        start = place
-                        end   = i
-                    for j in range(start,end):
-                        self.swap(j, j+1)
+                        for j in range(place,i):
+                            self.swap_qubits(j, j+1)
+                    
                 
 
-    def comtract_two_qubits(self, runingTensor, indices):
+    def contract_two_qubits(self, runingTensor, indices):
         #function to combine two qubits
-        if len(self.Lambda[indices1]) == 1:
+        if len(self.Lambda[indices]) == 1:
             return np.tensordot(np.tensordot(runingTensor,self.Lambda[indices-1],0),self.qbit[indices],0)
         else:
             return np.tensordot(np.tensordot(runingTensor,self.Lambda[indices-1],1),self.qbit[indices],1)
@@ -106,7 +111,7 @@ class MPS:
         self.reorder
         out = self.qbit[0]
         for i in range(1,self.N):
-            out = comtract_two_qubits(out, i)
+            out = self.contract_two_qubits(out, i)
         return out
 
         
